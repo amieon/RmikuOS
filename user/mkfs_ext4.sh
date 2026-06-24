805b6e8d7db2833a3f5ea6417a66d3cf0bfab431
Aimiku <wtmsb3770@gmail.com>
2026-06-25 00:20:26 +0800

    [refactor] 把这些偏系统的可执行文件(la,cat,echo)放到一个文件夹，平常的哪些test文件放到另一个文件夹，其中偏系统的可以想哪里调用就哪里调用，像环境变量一样，其他的test就比较无所谓了，但是为了兼容前面的调度器实验，也是想哪里调用就哪里调用

--- user/mkfs_ext4.sh
+++ user/mkfs_ext4.sh
@@ -23,7 +23,7 @@ else
 fi
 
 # 确保基础目录存在
-mkdir -p "$ROOT/bin" "$ROOT/etc" "$ROOT/home" "$ROOT/tmp" "$ROOT/dev" "$ROOT/proc"
+mkdir -p "$ROOT/bin" "$ROOT/etc" "$ROOT/home" "$ROOT/tmp" "$ROOT/dev" "$ROOT/proc" "$ROOT/tests"
 
 # 如果用户没有提供 motd，就生成默认 motd
 if [ ! -f "$ROOT/etc/motd" ]; then
@@ -33,14 +33,20 @@ EOF
 fi
 
 # 把编译出来的用户程序放进 /bin
-for f in user/build/${ARCH}/*.elf; do
+
+for f in user/build/${ARCH}/bin/*.elf; do
   [ -e "$f" ] || continue
-
   base="$(basename "$f" .elf)"
-
-  clean="$(printf "%s" "$base" | sed -E 's/^([0-9]+_)+//')"
-
+  clean="$(printf "%s" "$base" | sed -E 's/^([0-9]+_)+//')" 
   cp "$f" "$ROOT/bin/$clean"
+done
+
+
+for f in user/build/${ARCH}/tests/*.elf; do
+  [ -e "$f" ] || continue
+  base="$(basename "$f" .elf)"          
+  clean="$(printf "%s" "$base" | sed -E 's/^([0-9]+_)+//')"  
+  cp "$f" "$ROOT/tests/$clean"
 done
 
 # 5. 简单展示 rootfs 内容
