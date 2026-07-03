9249ba668efe69c949b5f4ee4a9ed18737768d8c
amieon <wtmsb3770@gmail.com>
2026-07-03 17:45:27 +0800

    [feat(signal)] 加入信号处理函数do_signal，系统调用返回前和遇到用户态非法指令时处理

--- user/include/signal.h
+++ user/include/signal.h
@@ -5,7 +5,12 @@ extern "C" {
 #include "types.h"
 #include "syscall.h"
 
-#define SIGINT 2
+#define SIGINT  2
+#define SIGILL  4
+#define SIGABRT 6
+#define SIGFPE  8
+#define SIGKILL 9
+#define SIGTERM 15
 
 void kill(isize pid, usize sig){
     syscall3(SYS_KILL, pid, sig, 0);
