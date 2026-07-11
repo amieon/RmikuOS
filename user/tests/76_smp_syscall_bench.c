00307a49c05b80e96b6e63876ce51cfe83df8b99
amieon <wtmsb3770@gmail.com>
2026-07-12 01:02:43 +0800

    [test(SMP)] smp_syscall_bench.c：syscall/调度噪声测试 目标：测“多核下 syscall 密集型程序为什么变慢”。

---
+++ user/tests/76_smp_syscall_bench.c
@@ -0,0 +1,90 @@
+#include "user.h"
+
+int my_atoi(const char *str)
+{
+    int sign = 1;
+    int result = 0;
+
+
+    // 跳过空格
+    while (*str == ' ' ||
+           *str == '\t' ||
+           *str == '\n')
+    {
+        str++;
+    }
+
+
+    // 符号
+    if (*str == '-')
+    {
+        sign = -1;
+        str++;
+    }
+    else if (*str == '+')
+    {
+        str++;
+    }
+
+
+    // 数字
+    while (*str >= '0' &&
+           *str <= '9')
+    {
+        result = result * 10 +
+                 (*str - '0');
+
+        str++;
+    }
+
+
+    return result * sign;
+}
+
+int main(int argc, char **argv) {
+    int workers = 1;
+    int loops = 10000;
+
+    if (argc >= 2) {
+        workers = my_atoi(argv[1]);
+    }
+
+    if (argc >= 3) {
+        loops = my_atoi(argv[2]);
+    }
+
+    printf("[smp_syscall] workers=%d loops=%d\n", workers, loops);
+
+    long start = get_ticks();
+
+    for (int i = 0; i < workers; i++) {
+        int pid = fork();
+
+        if (pid < 0) {
+            printf("[smp_syscall] fork failed\n");
+            return 1;
+        }
+
+        if (pid == 0) {
+            volatile int sum = 0;
+
+            for (int j = 0; j < loops; j++) {
+                sum += getpid();
+            }
+
+            exit(sum & 0xff);
+        }
+    }
+
+    int status = 0;
+    for (int i = 0; i < workers; i++) {
+        waitpid(&status);
+    }
+
+    long end = get_ticks();
+
+    printf("[smp_syscall] done workers=%d elapsed_ticks=%ld\n",
+           workers, end - start);
+
+    return 0;
+}
