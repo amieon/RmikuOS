90cc4394374d65ff60abad5730898a6aac18de8f
amieon <wtmsb3770@gmail.com>
2026-07-23 20:47:06 +0800

    [feat(shell)] 加入记录运行时间的命令

---
+++ user/src/run_tick_and_time.c
@@ -0,0 +1,51 @@
+#include "user.h"
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("usage: tick <cmd> [args...]\n");
+        return 1;
+    }
+
+    long tick0 = get_ticks();
+    long time0 = get_time();
+    int pid = fork();
+    if (pid == 0) {
+        struct exec_args args;
+        int n = argc - 1;
+        if (n > EXEC_MAX_ARGS) {
+            n = EXEC_MAX_ARGS;
+        }
+        args.argc = (usize)n;
+        for (int i = 0; i < EXEC_MAX_ARGS; i++) {
+            if (i < n) {
+                args.argv[i].ptr = argv[1 + i];
+                args.argv[i].len = strlen(argv[1 + i]);
+            } else {
+                args.argv[i].ptr = 0;
+                args.argv[i].len = 0;
+            }
+        }
+        exec_with_args(argv[1], &args);
+        printf("[tick] exec failed\n");
+        exit(127);
+    }
+
+    int status = 0;
+    waitpid(pid, &status, WUNTRACED);    
+    long tick1 = get_ticks();
+    long time1 = get_time();
+
+    printf("[tick] ");
+    printf(argv[1]);
+    printf(" = ");
+    printf("%d", (int)(tick1 - tick0));
+    printf(" ticks\n");
+    printf("[time] ");
+    printf(argv[1]);
+    printf(" = ");
+    printf("%d", (int)(time1 - time0));
+    printf(" ns\n exit=");
+    printf("%d", status);
+    printf("\n");
+    return 0;
+}
---
+++ user/src/run_time.c
@@ -0,0 +1,44 @@
+#include "user.h"
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("usage: tick <cmd> [args...]\n");
+        return 1;
+    }
+
+    long t0 = get_time();
+    int pid = fork();
+    if (pid == 0) {
+        struct exec_args args;
+        int n = argc - 1;
+        if (n > EXEC_MAX_ARGS) {
+            n = EXEC_MAX_ARGS;
+        }
+        args.argc = (usize)n;
+        for (int i = 0; i < EXEC_MAX_ARGS; i++) {
+            if (i < n) {
+                args.argv[i].ptr = argv[1 + i];
+                args.argv[i].len = strlen(argv[1 + i]);
+            } else {
+                args.argv[i].ptr = 0;
+                args.argv[i].len = 0;
+            }
+        }
+        exec_with_args(argv[1], &args);
+        printf("[tick] exec failed\n");
+        exit(127);
+    }
+
+    int status = 0;
+    waitpid(pid, &status, WUNTRACED);    
+    long t1 = get_time();
+
+    printf("[time] ");
+    printf(argv[1]);
+    printf(" = ");
+    printf("%d", (int)(t1 - t0));
+    printf(" ns, exit=");
+    printf("%d", status);
+    printf("\n");
+    return 0;
+}
