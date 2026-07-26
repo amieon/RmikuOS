86a9bfd6e42f4adb89b9b2925a8fa84b3781f2a4
amieon <wtmsb3770@gmail.com>
2026-07-27 02:51:34 +0800

    [exp(sched)] 完成实验4的period=6的实验

--- user/sched/sexp4_dyn.c
+++ user/sched/sexp4_dyn.c
@@ -26,7 +26,7 @@ static void sl_reset_state(void) {
 }
 
 static void setup(void) {
-    sl_add_jobs_parent("ctrl", 300, 1, /*period*/6, /*cpu*/2, /*burn*/400000);
+    sl_add_jobs_parent("ctrl", 300, 1, /*period*/5, /*cpu*/2, /*burn*/400000);
     sl_add_spin_phased("ai", 100, 150, 12000, /*light_active*/3);
     sl_add_spin("log", 50, 32, 12000);
 }
@@ -57,7 +57,7 @@ int main(void) {
     const unsigned long total = 72000;
     const int nreps = 3;
     const int fixed_alphas[] = {0, 30, 60, 100};
-    const int aimd_alphas[]  = {0, 50, 100};
+    const int aimd_alphas[]  = {10, 50, 100};
 
     printf("# sexp4_dyn_v2: dyn load x fixed{0,30,60,100} + aimd{0,50,100} x (1w+%dr), total=%lu\n",
            nreps, total);
