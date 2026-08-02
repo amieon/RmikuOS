1f6abd31454af4e9e9d31c9b56408191fdbdfba6
amieon <wtmsb3770@gmail.com>
2026-08-03 02:20:19 +0800

    [fix(errno)] lua.o 的强定义 errno +，那我string.o 就弱定义errno吧

--- user/lib/string.c
+++ user/lib/string.c
@@ -1,6 +1,6 @@
 
 /* errno: POSIX 全局错误码(errno.h 已声明, 这里提供定义) */
-int errno = 0;
+int errno __attribute__((weak)) = 0; 
 
 void *memset(void *s, int c, unsigned long n) {
     unsigned char *p = (unsigned char *)s;
